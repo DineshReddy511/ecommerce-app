8c46e3019f8995109fd23ac5c79da0c658c89f59
DineshReddy511 <dineshreddycmd@gmail.com>
2021-04-12 20:15:25 +0530

    Adding Update and Delete API's

--- ecommerce/src/main/java/com/mysnap/controllers/PublicUserController.java
+++ ecommerce/src/main/java/com/mysnap/controllers/PublicUserController.java
@@ -1,11 +1,17 @@
 package com.mysnap.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mysnap.entities.UserEntity;
 import com.mysnap.pojos.User;
 import com.mysnap.services.IUserService;
 
@@ -15,9 +21,30 @@ public class PublicUserController {
 
 	@Autowired
 	private IUserService userService;
-	
+
 	@PostMapping("/")
-	public User saveUser(@RequestBody User user) {
+	public UserEntity saveUser(@RequestBody User user) throws Exception {
 		return userService.saveUser(user);
+	}
+
+	@GetMapping("/all")
+	public Iterable<UserEntity> getAllUser() {
+		return userService.getAllUsers();
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> getUser(@PathVariable("id") int id) {
+		return userService.getUser(id);
+	}
+	@PutMapping
+	public void updateUser(@RequestBody User user) throws Exception
+	{
+		userService.updateUser(user);
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public void deleteUser(@PathVariable("id") int id)
+	{
+		userService.deleteUser(id);
 	}
 }
---
+++ ecommerce/src/main/java/com/mysnap/exceptions/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.mysnap.exceptions;
+
+public class BadRequestException extends Exception {
+
+	
+	private static final long serialVersionUID = 1L;
+
+}
